fd9a1dfc2ac33dafb9b2c785883fcc0f79c8ad3c
tsar-saras <sarassardessai@gmail.com>
2025-01-15 20:00:08 +0530

    SerialToServo.ino

--- SerialToServo.ino
+++ SerialToServo.ino
@@ -14,29 +14,19 @@ Adafruit_PWMServoDriver pwm2 = Adafruit_PWMServoDriver(0x41);
 int angleToPulse(int angle) {
   return map(angle, 0, 180, SERVOMIN, SERVOMAX);
 }
-void servocom(int x, int y, int z, int lor){
+void servocom(int x, int y, int lor){
   int ang = lor==0?45:135;
   digitalWrite(x+20,HIGH);
-   digitalWrite(z+20,HIGH);
   if(x>10)
    pwm2.setPWM(x-11, 0, angleToPulse(ang));
  else
   pwm1.setPWM(x-1, 0, angleToPulse(ang));
-  if(z>10)
-  pwm2.setPWM(z-11, 0, angleToPulse(ang));
-  else
-   pwm1.setPWM(z-1, 0, angleToPulse(ang));
-  delay(y);
+ delay(y);
    if(x>10)
    pwm2.setPWM(x-11, 0, angleToPulse(90));
  else
   pwm1.setPWM(x-1, 0, angleToPulse(90));
-  if(z>10)
-  pwm2.setPWM(z-11, 0, angleToPulse(90));
-  else
-   pwm1.setPWM(z-1, 0, angleToPulse(90));
    digitalWrite(x+20,LOW);
-   digitalWrite(z+20,LOW);
   }
 
 
@@ -61,25 +51,21 @@ while(k>=0){
 void loop() {
 
 String myString= Serial.readString();
-//string format  is servonumber|servonumber|delay|1 for ACW and 0 for CW   eg. 011210001
+//string format  is servonumber|servonumber|delay|1 for ACW and 0 for CW   eg. 0110001
 //format for servo numbers, is 01 for first servo of first driver and 11 for first servo of second driver,
 //for using only one servo at a time give 00.
   
  String subString1 = myString.substring(0, 2);
-  Serial.println(subString1);  
- String subString3 = myString.substring(2, 4);
-   Serial.println(subString3); 
- String subString2 = myString.substring(4,8);
-  Serial.println(subString2);  
- String subString4 = myString.substring(8, 9);
-   Serial.println(subString4); 
+  //Serial.println(subString1);  
+ String subString2 = myString.substring(2,6);
+  //Serial.println(subString2);  
+ String subString4 = myString.substring(6, 7);
+   //Serial.println(subString4); 
 int x;
 int y;
-int z;
 int lor;
 x = subString1.toInt(); // servo1
 y = subString2.toInt(); // delay
-z = subString3.toInt(); // servo2
 lor = subString4.toInt(); //cw or acw
-servocom(x,y,z,lor);
+servocom(x,y,lor);
 }
